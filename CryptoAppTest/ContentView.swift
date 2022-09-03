0789b6c985e5f27f5f22426fa594da1ee57339b5
2whitewolf <48834435+2whitewolf@users.noreply.github.com>
2022-09-03 16:54:21 +0300

    add frameworks

---
+++ CryptoAppTest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CryptoAppTest
+//
+//  Created by Bogdan Sevcenco on 03.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoAppTest/CryptoAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppTestApp.swift
+//  CryptoAppTest
+//
+//  Created by Bogdan Sevcenco on 03.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
